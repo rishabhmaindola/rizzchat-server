ea2c814261f8e54720959061a30ca45995aca93d
Rishabh Maindola <143874827+rishabhmaindola@users.noreply.github.com>
2024-02-07 21:24:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ let connectedUsernames = [];
 
 const io = new Server(server, {
   cors: {
-    origin: `https://rizzchat-server-rishabhmaindolas-projects.vercel.app/`,
+    origin: `https://rizzchat-rishabhmaindolas-projects.vercel.app/`,
     methods: ["GET", "POST"],
   },
 });
